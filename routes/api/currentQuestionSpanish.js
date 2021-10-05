c4512d76f0002a102580f936182118ee4dfff3a1
iuzairazizDOT <90470177+iuzairazizDOT@users.noreply.github.com>
2021-10-05 17:40:46 +0500

    Update currentQuestionSpanish.js

--- routes/api/currentQuestionSpanish.js
+++ routes/api/currentQuestionSpanish.js
@@ -35,7 +35,7 @@ router.post("/", auth, async (req, res) => {
       `
     username : ${req.body.userName}<br>
     click on below link to view changed questions<br>
-    https://fe-school-wellness-app-zmxnd.ondigitalocean.app/question-spanish
+    https://fe-school-wellness-app-ce562.ondigitalocean.app/question-spanish
     `
     );
     await question.save();
